fc04f48a92b71bd022e8d6239e346e569244da5e
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-10-09 12:03:53 +0300

    Lesson 15

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,22 @@ import React, { PureComponent } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 
 export default class App extends PureComponent {
+    constructor() {
+        super();
+        this.state = {
+            text: ''
+        }
+    }
     render() {
         return (
             <div>
                 <h2>Note to self</h2>
-                <Form>
-                    <FormControl />
-                    <Button>Submit</Button>
+                <Form inline>
+                    <FormControl onChange={event => {this.setState({ text: event.target.value }) }}/>
+                    {'  '}
+                    <Button onClick={() => { console.log(`state: ${this.state.text}`) }}>
+                        Submit
+                    </Button>
                 </Form>
             </div>
         );
